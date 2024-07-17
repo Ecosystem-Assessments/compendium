dacb1b584ab61643558717b332fa6a463343b326
David Beauchesne <david.beauchesne@uqar.ca>
2024-07-17 15:52:07 -0400

    quick updateto structure and tools

--- R/_pipeline.R
+++ R/_pipeline.R
@@ -5,19 +5,20 @@
 pipeline <- function() {
   # Update global parameters
   global_parameters()
-  
-  # Get area of interest & basemaps
-  get_aoi()
-  get_basemap()
 
-  # Integrate data 
-  pipedat::pipeflow()
-  
-  # Figures 
-  fig_aoi()
-  
-  # Report and publications 
-  render_frontpate()
+  # Get area of interest & basemaps
+  get_basemap()
+  get_aoi()
+
+  # Integrate data
+  library(pipedat)
+  pipeflow()
+
+  # Figures
+  # fig_aoi()
+
+  # Report and publications
+  # render_frontpate()
   render_report()
   # render_webinar()
 }
